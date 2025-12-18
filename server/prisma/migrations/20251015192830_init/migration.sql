62c48fb977da4540d438c5df2d52ad3ee6aea7da
Norman Chang <sangheilios081@gmail.com>
2025-12-17 18:07:33 -0800

    Just deleted the SQLite Migrations and switched providers to PostgreSQL

--- server/prisma/migrations/20251015192830_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Job" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "company" TEXT NOT NULL,
-    "position" TEXT NOT NULL,
-    "status" TEXT NOT NULL DEFAULT 'applied',
-    "appliedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "notes" TEXT
-);
